6266a1011662a774db5b48e1168a9765da53048a
khalil <khalillakhdharatc@gmail.com>
2023-04-30 13:40:26 +0100

    modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  { path: 'compte', loadChildren: () => import('./compte/compte.module').then(m => m.CompteModule) }, // simple loading
+  // lazy loading for notfound component from app.module module
+  {path: '**',component:NotfoundComponent},
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
